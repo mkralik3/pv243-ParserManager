e38308aa2fdc0ce0b51977020809eaf7792cb9c6
mimaboci <michaelabocanova@gmail.com>
2018-05-24 17:51:38 +0200

    parser-job

---
+++ service/src/main/java/cz/fi/muni/pv243/service/batching/controller/ParserBatchController.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.fi.muni.pv243.service.batching.controller;
+
+/**
+ *
+ * @author Michaela Bocanova
+ */
+public interface ParserBatchController {
+    
+    public void startJob();
+}
