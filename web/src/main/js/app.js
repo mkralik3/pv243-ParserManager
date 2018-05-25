50d9ddafdd398fb01e82592c0179689355b5cc2a
Sevitas <sevcik.martin4@gmail.com>
2018-05-25 15:07:54 +0200

    React integrated

---
+++ web/src/main/js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// tag::vars[]
+const React = require('react');
+const ReactDOM = require('react-dom');
+const client = require('./client');
+// end::vars[]
+
+// tag::app[]
+class App extends React.Component {
+
+
+}
