2c3351063b5ef566b36237a0226fc26123eb5e16
mkralik3 <mkralik@redhat.com>
2018-06-03 15:38:39 +0200

    Create parser test for rest

---
+++ rest/src/test/java/cz.fi.muni.pv243.rest/Configuration.java
@@ -0,0 +1,38 @@
+package cz.fi.muni.pv243.rest;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenFormatStage;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenResolverSystem;
+
+import java.io.File;
+
+public class Configuration {
+
+    public static final String WAR_NAME = "test-rest";
+
+    public static WebArchive deployment() {
+
+
+        MavenResolverSystem resolver = Maven.resolver();
+        MavenFormatStage dependencies = resolver
+                .loadPomFromFile("pom.xml")
+                .importCompileAndRuntimeDependencies()
+                .importTestDependencies()
+                .resolve()
+                .withTransitivity();
+
+        final WebArchive archive = ShrinkWrap
+                .create(WebArchive.class, WAR_NAME + ".war")
+                .addPackages(true, "cz.fi.muni.pv243.rest")
+                .addAsLibraries(dependencies.asFile())
+                .setWebXML("web.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+        archive.as(ZipExporter.class).exportTo(new File("/tmp/" + archive.getName()), true);
+
+        return archive;
+    }
+}
